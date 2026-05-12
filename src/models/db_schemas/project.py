0283562c59959b0cb0193cf534caf2261866c101
aymen <aymenbenzlaouia95@gmail.com>
2026-05-12 18:29:29 +0100

    add mongo and save chunks to it

---
+++ src/models/db_schemas/project.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from bson import ObjectId
+from pydantic import BaseModel, Field, validator
+
+
+class Project(BaseModel):
+    id: Optional[ObjectId] = Field(None, alias="_id")
+    project_id: str = Field(..., min_length=1)
+
+    @validator("project_id")
+    def validate_project_id(cls, value):
+        if not value.isalnum():
+            raise ValueError("project_id must be alphanumeric")
+        return value
+
+    class Config:
+        arbitrary_types_allowed = True
--- src/routes/data.py
+++ src/routes/data.py
@@ -1,13 +1,16 @@
 import logging
 
 import aiofiles
-from fastapi import APIRouter, Depends, UploadFile, status
+from fastapi import APIRouter, Depends, Request, UploadFile, status
 from fastapi.responses import JSONResponse
 
 from controllers import DataController
 from controllers.ProcessController import ProcessController
 from helpers.config import Settings, get_settings
 from models import ResponseSignal
+from models.ChunkModel import ChunkModel
+from models.db_schemas import DataChunk
+from models.ProjectModel import ProjectModel
 
 from .schemas.data import ProcessRequest
 
@@ -17,9 +20,14 @@ data_router = APIRouter(prefix="/api/v1/data", tags=["api_v1", "data"])
 
 @data_router.post("/upload/{project_id}")
 async def upload_data(
-    project_id: str, file: UploadFile, app_settings: Settings = Depends(get_settings)
+    request: Request,
+    project_id: str,
+    file: UploadFile,
+    app_settings: Settings = Depends(get_settings),
 ):
+    project_model = ProjectModel(db_client=request.app.db_client)
 
+    project = await project_model.get_project_or_create_one(project_id)
     data_controller = DataController()
     # validate the file properties
     is_valid, result_signal = data_controller.validate_uploaded_file(file=file)
@@ -45,14 +53,25 @@ async def upload_data(
             content={"signal": ResponseSignal.FILE_UPLOAD_FAILED.value},
         )
 
-    return JSONResponse(content={"signal": result_signal, "file_id": file_id})
+    return JSONResponse(
+        content={
+            "signal": result_signal,
+            "file_id": file_id,
+        }
+    )
 
 
 @data_router.post("/process/{project_id}")
-async def process_endpoint(project_id: str, process_request: ProcessRequest):
+async def process_endpoint(
+    request: Request, project_id: str, process_request: ProcessRequest
+):
     file_id = process_request.file_id
     chunk_size = process_request.chunk_size
     overlap_size = process_request.overlap_size
+    do_reset = process_request.do_reset
+
+    project_model = ProjectModel(db_client=request.app.db_client)
+    project = await project_model.get_project_or_create_one(project_id)
 
     process_controller = ProcessController(project_id=project_id)
 
@@ -71,4 +90,26 @@ async def process_endpoint(project_id: str, process_request: ProcessRequest):
             content={"signal": ResponseSignal.PROCESSING_FAILED.value},
         )
 
-    return file_chunks
+    file_chunks_records = [
+        DataChunk(
+            chunk_text=chunk.page_content,
+            chunk_metadata=chunk.metadata,
+            chunk_order=i + 1,
+            chunk_project_id=project.id,
+        )
+        for i, chunk in enumerate(file_chunks)
+    ]
+
+    chunk_model = ChunkModel(db_client=request.app.db_client)
+
+    if do_reset == 1:
+        _ = await chunk_model.delete_chunks_by_project_id(project_id=project.id)
+
+    no_records = await chunk_model.insert_many_chunks(chunks=file_chunks_records)
+
+    return JSONResponse(
+        content={
+            "signal": ResponseSignal.PROCESSING_SUCCESS.value,
+            "inserted_chunks": no_records,
+        }
+    )
