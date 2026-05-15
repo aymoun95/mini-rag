b26a8f52ba93ceb14f7eec1a285c7c69b4216d38
aymen <aymenbenzlaouia95@gmail.com>
2026-05-15 11:27:20 +0100

    add indexes

--- src/routes/data.py
+++ src/routes/data.py
@@ -25,9 +25,9 @@ async def upload_data(
     file: UploadFile,
     app_settings: Settings = Depends(get_settings),
 ):
-    project_model = ProjectModel(db_client=request.app.db_client)
+    project_model = await ProjectModel.create_instance(db_client=request.app.db_client)
 
-    project = await project_model.get_project_or_create_one(project_id)
+    _project = await project_model.get_project_or_create_one(project_id)
     data_controller = DataController()
     # validate the file properties
     is_valid, result_signal = data_controller.validate_uploaded_file(file=file)
@@ -70,7 +70,7 @@ async def process_endpoint(
     overlap_size = process_request.overlap_size
     do_reset = process_request.do_reset
 
-    project_model = ProjectModel(db_client=request.app.db_client)
+    project_model =await ProjectModel.create_instance(db_client=request.app.db_client)
     project = await project_model.get_project_or_create_one(project_id)
 
     process_controller = ProcessController(project_id=project_id)
@@ -100,7 +100,7 @@ async def process_endpoint(
         for i, chunk in enumerate(file_chunks)
     ]
 
-    chunk_model = ChunkModel(db_client=request.app.db_client)
+    chunk_model =await ChunkModel.create_instance(db_client=request.app.db_client)
 
     if do_reset == 1:
         _ = await chunk_model.delete_chunks_by_project_id(project_id=project.id)
