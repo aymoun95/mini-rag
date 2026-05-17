4c77ffd4bc05cc9c7340eac68fc5c6e5c24867df
aymen <aymenbenzlaouia95@gmail.com>
2026-05-17 18:19:12 +0100

    refactor file processing

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -22,6 +22,9 @@ class ProcessController(BaseController):
         file_ext = self.get_file_extension(file_id)
         file_path = os.path.join(self.project_path, file_id)
 
+        if not os.path.exists(file_path):
+            return None
+
         if file_ext == ProcessingEnum.TXT.value:
             return TextLoader(file_path, "utf-8")
 
@@ -32,7 +35,9 @@ class ProcessController(BaseController):
 
     def get_file_content(self, file_id: str):
         loader = self.get_file_loader(file_id=file_id)
-        return loader.load()
+        if loader:
+            return loader.load()
+        return None
 
     def process_file_content(
         self,
